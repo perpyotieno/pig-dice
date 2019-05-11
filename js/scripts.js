7adea92586765c0bcb3cb2835951b07ddbd4f61f
Perpetua Mary <perpetuamary@yahoo.com>
2019-05-11 13:16:29 +0300

    Add constructor function for player.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var player1, player2;
+
+function Player(name,diceRoll,turnScore,totalScore,active){
+  this.name= name;
+  this.diceRoll= 0;
+  this.turnScore=0;
+  this.totalScore=0;
+  this.active= active;
+}
