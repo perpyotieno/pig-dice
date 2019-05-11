3034f57c393bfb715c952206e937ddd06ff05f10
Perpetua Mary <perpetuamary@yahoo.com>
2019-05-11 14:45:44 +0300

    Add a function that disables one player's field when it is time for the other player to play. Add css rule that makes an inactive players feel translucent

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 var player1, player2;
-
+// constructor function for player
 function Player(name,diceRoll,turnScore,totalScore,active){
   this.name= name;
   this.diceRoll= 0;
   this.turnScore=0;
   this.totalScore=0;
   this.active= active;
+}
+// function to make the gaming area for each player active or inactive depending on their turn
+function activePlayer(){
+  if(player1.active===true && player2.active===false){
+    $(".firstPlayer").children().prop("disabled",false);
+    $(".firstPlayer").removeClass("disableGamingField");
+    $(".secondPlayer").children().prop("disabled",true);
+    $(".secondPlayer").addClass("disableGamingField");
+  }
+  else{
+    $(".firstPlayer").children().prop("disabled",true);
+    $(".firstPlayer").addClass("disableGamingField");
+    $("secondPlayer").children().prop("disabled",false);
+    $(".secondPlayer").removeClass("disableGamingField");
+  }
 }
